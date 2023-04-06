c8e6b6a2587b99db8a7f2224f39b2a2b78a10f6c
demishu <101947576+demishu@users.noreply.github.com>
2023-04-06 16:55:42 +0200

    first_commit

--- generic_settings.py
+++ generic_settings.py
@@ -1,8 +1,8 @@
 import os
 app_name = "CashFlowRecorder"
 app_root_path = os.path.expanduser(f"~/Documents/{app_name}/")
-folder_tuple=("database",)
+folders = ("database",)
 if not os.path.exists(app_root_path):
     os.makedirs(app_root_path)
-    for folder in folder_tuple:
+    for folder in folders:
         os.makedirs(app_root_path+folder)
