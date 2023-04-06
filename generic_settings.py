1e895a88dc453a47e166c8837b199cce68488883
demishu <101947576+demishu@users.noreply.github.com>
2023-04-06 17:14:17 +0200

    first_commit

--- generic_settings.py
+++ generic_settings.py
@@ -1,8 +1,14 @@
-import os
+import pathlib
+
 app_name = "CashFlowRecorder"
-app_root_path = os.path.expanduser(f"~/Documents/{app_name}/")
-folders = ("database",)
-if not os.path.exists(app_root_path):
-    os.makedirs(app_root_path)
+
+app_root_path = pathlib.Path.home() / "Documents" / app_name  # 软件根目录
+app_root_path.mkdir(parents=True, exist_ok=True)
+
+folders = ("database",)  # 需要创建的文件夹列表
+
+is_empty = not any(app_root_path.iterdir())
+if is_empty:  # 如果根目录下没有任何文件/文件夹，则生成以下文件夹
     for folder in folders:
-        os.makedirs(app_root_path+folder)
+        tmp = app_root_path / folder
+        tmp.mkdir(parents=True, exist_ok=True)
